321d9cf82954b81d2a0c1c31066e01214ad199ac
wanderlustcamis <germanophile@gmail.com>
2020-10-22 12:35:07 -0300

    second commit

---
+++ java/js.js
@@ -0,0 +1,10 @@
+var numero = prompt ("add a number");
+if (numero > 1000)
+{  alert ("hey!") }
+var texto = prompt ("say hi!");
+if (texto = "hi")
+  { console.log = ("they said hi!!")  }
+var masnumeros = prompt ("add a number");
+  masnumeros = Number(masnumeros);
+  if (masnumeros > 10 && masnumeros < 50)
+  {   alert ("yey!");}
