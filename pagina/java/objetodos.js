5e88331e7fdf2d09a2b39e5202706f8209a480fe
wanderlustcamis <germanophile@gmail.com>
2020-11-11 18:17:40 -0300

    fixed calculator

--- pagina/java/objetodos.js
+++ pagina/java/objetodos.js
@@ -1,5 +1,10 @@
 var nh = new health ("; eat well", " excersise", " meditate")
+var r = document.getElementById("resultado")
 
   function ver () {
-    alert("Healthy lifestyle" + nh.food +","+ nh.life + "and" + nh.living)
+    alert("Healthy lifestyle" + nh.food +","+ nh.life + " and" + nh.living)
   }
+
+  function cal(){
+    r.alert = c.cal
+}
--- pagina/java/objetos.js
+++ pagina/java/objetos.js
@@ -8,3 +8,24 @@ healthy.living ="exercise" ;
     this.life=life;
     this.living=living;
 }
+this.cal = function (n1, ope, n2) {
+  n1= Number(n1)
+  n2= Number (n2)
+    switch (ope){
+      case "+":
+        return n1 + n2
+        break
+      case "-":
+        return n1 - n2
+        break
+      case "*":
+        return n1 * n2
+        break
+      case "/":
+        return n1 / n2
+        break
+      default:
+       console.error("NAN")
+    }
+
+}
