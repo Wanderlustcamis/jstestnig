117f198653bea234712128036583b34e2ffcf64b
wanderlustcamis <germanophile@gmail.com>
2020-11-14 22:45:23 -0300

    arregle yoga.js

--- pagina/java/function.js
+++ pagina/java/function.js
@@ -1,6 +1,7 @@
-var n1 = prompt ("add a number")
-var n2 = prompt ("add another number")
- 
+
   function promedio (num1, num2){
     return (Number (num1) + Number (num2))/2
   }
+
+  var n1 = prompt ("add a number")
+  var n2 = prompt ("add another number")
