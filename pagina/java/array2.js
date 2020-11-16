2a2986cff01462ab32ef3903d11c73aca96e9472
wanderlustcamis <germanophile@gmail.com>
2020-11-16 02:00:37 -0300

    Add extra var

--- pagina/java/array2.js
+++ pagina/java/array2.js
@@ -1,6 +1,7 @@
 function med(){
   var m = ['meditate 20 minutes twice a day'];
-  var t =[' for a better spiritual growth'];
+  var t =[' for a better spiritual growth.'];
+  var y = " you can do it";
   var mix = m.concat(t);
-  alert (mix)
+  alert (mix + y)
 }
