08cad0072f7d52ec6a2d70467f405f34e7b0dfef
wanderlustcamis <germanophile@gmail.com>
2021-01-02 20:36:24 -0300

    plans

---
+++ pagina/java/plan.js
@@ -0,0 +1,32 @@
+window.onload = function plan(){
+  document.getElementById('plan')
+  var plan = ["yoga", "meditation", "Exercise", "yoga meditation", "yoga excercise"]
+
+    for (var i=0 ; i< plan.length; i++){
+      if (i=0){
+        document.CreateElement("p")
+        title.id = "plan"
+        title.innerText = "15 usd"
+      }
+      if (i=1){
+        document.CreateElement("p")
+        title.id = "plan"
+        title.innerText = "15 usd"
+      }
+      if (i=2){
+        document.CreateElement("p")
+        title.id = "plan"
+        title.innerText = "19 usd"
+      }
+      if (i=3){
+        document.CreateElement("p")
+        title.id = "plan"
+        title.innerText = "28 usd"
+      }
+      if (i=4){
+        document.CreateElement("p")
+        title.id = "plan"
+        title.innerText = "30 usd"
+      }
+    }
+}
